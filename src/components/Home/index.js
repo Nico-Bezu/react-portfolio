fe4d003e1829ec96086a43bcaef9b53195575572
Nicodemus <nbezuayeho@gmail.com>
2024-02-10 15:42:28 -0800

    finished building the home page & animated letters

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,18 +1,39 @@
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom'
-import LogoN from '../../assets/images/logo-n.png'
-import LogoSub from '../../assets/images/logo-n.png'
 import './index.scss'
+import AnimatedLetters from '../AnimatedLetters'
 
 const Home = () => 
 {
+    const [letterClass, setLetterClass] = useState('text-animate')
+    const nameArray = [' ', 'N', 'i', 'c','o','d','e','m','u','s']
+    const studentArray = ['S','t','u','d','e','n','t']
+
+    useEffect(() => {
+        const timerId = setTimeout(() => {
+            setLetterClass('text-animate-hover');
+        }, 4000);
+    
+        return () => clearTimeout(timerId);
+    }, []);
+    
 
     return (
         <div className="container home-page">
             <div className="text-zone">
-                <h1>Hi, <br /> I'm
-                Nicodemus 
+                <h1>
+                    <span className={letterClass}>H</span> 
+                    <span className={`${letterClass} _12`}>i,</span> 
+                    <br />
+                    <span className={`${letterClass} _13`}>I</span> 
+                    <span className={`${letterClass} _14`}>'m</span> 
+                <AnimatedLetters letterClass={letterClass}
+                strArray = {nameArray}
+                idx = {15} />
                 <br />
-                Student at a top 3 university for CS
+                <AnimatedLetters letterClass={letterClass}
+                strArray = {studentArray}
+                idx = {22} />
                 </h1>
                 <h2> Software Engineer / Full Stack Developer </h2>
                 <Link to="/contact" className="flat-button"> CONTACT ME</Link>
